030c671a2d85aa43c9ad8f99864b9c4c9831dcdd
“bhumiii10” <2994327a@student.gla.ac.uk>
2025-03-07 13:32:14 +0000

    Fixed the base templates and refactored the code

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from restaurants.models import Restaurant
-
-def restaurants(request):
-    restaurants = Restaurant.objects.all()
-    return render(request, 'restaurants.html', {'restaurants': restaurants})
+# restaurants/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Restaurant, MenuItem
 
 def home(request):
     return render(request, 'home.html')
+
+def restaurant_list(request):
+    tag = request.GET.get('tag')
+    if tag:
+        restaurants = Restaurant.objects.filter(menu_items__dietary_tags__name=tag).distinct()
+    else:
+        restaurants = Restaurant.objects.all()
+    return render(request, 'restaurants/restaurant_list.html', {'restaurants': restaurants})
+
+def restaurant_detail(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    menu_items = MenuItem.objects.filter(restaurant=restaurant)
+    return render(request, 'restaurants/restaurant_detail.html', {'restaurant': restaurant, 'menu_items': menu_items})
